e0dba445f2f779e06e6124cf97e4ddb9e0e5ac5e
Jaii Mc <jaiimcz@gmail.com>
2012-04-24 22:48:43 +0100

    Derping it up!

--- src/com/github/Jaiimc/Derp/DerpCommandExecutor.java
+++ src/com/github/Jaiimc/Derp/DerpCommandExecutor.java
@@ -1,35 +1,32 @@
 package com.github.Jaiimc.Derp;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class DerpCommandExecutor implements CommandExecutor {
-public DerpCommandExecutor(Derp plugin) {
-}
+
+		@SuppressWarnings("unused")
+		private Derp plugin;
+	 
+		public DerpCommandExecutor(Derp plugin) {
+			this.plugin = plugin;
+		}
 
 @Override
 
 public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	Player player = null;
-	if (sender instanceof Player) {
-		player = (Player) sender;
-		
-	}
-	
-	if(cmd.getName().equalsIgnoreCase("Derp")) {
-		sender.sendMessage(ChatColor.GREEN + "You're a derp.");
-		return true;
-	} else if (cmd.getName().equalsIgnoreCase("Derpage")) {
-		if (player == null) {
-			sender.sendMessage(ChatColor.RED + "Oh shit, you need to be a player.");
-		} else {
-			sender.sendMessage(ChatColor.GREEN + "You're also a derp.");
-		}
-		return true;
-	}
+if ((sender instanceof Player) && (sender.hasPermission("Derp.derp"))) {
+	((Player)sender).getInventory().addItem(new ItemStack(Material.DIAMOND,64));
+	return true;
+} else {
+	sender.sendMessage(ChatColor.RED + "You must be a player!");
+	sender.sendMessage(ChatColor.RED + "You do not have permission to perform this command!");
 	return false;
+}
 }
 }
